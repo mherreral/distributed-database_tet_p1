dbceb8377f2f23b7fed84b05989ee9c4caa05ecd
Vincent Arcila <jandro.vins@gmail.com>
2022-04-02 15:56:19 -0500

    Finished post

--- router.py
+++ router.py
@@ -4,10 +4,12 @@ import json
 import cgi
 import pathlib
 import pickle
+import requests
 
 
 class Router():
     def __init__(self, DBSegmentsConfigFile="DBSegments", isReplica=False, replicaIP = None, segmentValueFile="SegmentValueTable.pickle"):
+        self.DBSegments = []
         self.readDBSegments(DBSegmentsConfigFile)
         self.isReplica = isReplica
         if not isReplica:
@@ -17,18 +19,20 @@ class Router():
         if self.segmentValueFile.exists():
             self.segmentValueTable = pickle.load(self.segmentValueFile.open("rb"))
         else:
-            self.segmentValueTable = []
+            self.segmentValueTable = {}
     def readDBSegments(self, configFile="DBSegments"):
         self.configFileDBSegments = pathlib.Path(configFile)
-        self.DBSegments = []
         with self.configFileDBSegments.open() as f:
             for line in f.readlines():
                 leaderAndFollowers = line.strip().split()
                 self.DBSegments.append(leaderAndFollowers)
             if len(self.DBSegments) == 0:
                 raise SystemExit('ERROR: not enough Database segments. Check database nodes config file.')
-    def nextSegment(self):
-        self.nextSegment +=1
+        print("Read DB segments")
+    def calculateNextSegment(self):
+        self.nextSegment = (self.nextSegment + 1) % len(self.DBSegments)
+        return self.nextSegment
+        
         return self.nextSegment
     def addKeyToSegmentValueTable(self, key, segment):
         self.segmentValueTable[key] = segment
@@ -39,6 +43,8 @@ class Router():
         
         
 
+router = Router()
+print ("ÁA")
 class RequestHandler(BaseHTTPRequestHandler):
 
     def acknowledge(self):
@@ -51,6 +57,7 @@ class RequestHandler(BaseHTTPRequestHandler):
     #    self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
     def do_POST(self):
+        global router
         ctype, pdict = cgi.parse_header(self.headers['content-type'])
         
         # refuse to receive non-json content
@@ -59,19 +66,34 @@ class RequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             return
 
-        content_length = int(self.headers['content-length'])
-        post_data = json.loads(self.rfile.read(content_length))
-        print(f"{type(post_data['value'])}")
+        contentLength = int(self.headers['content-length'])
+        postData = json.loads(self.rfile.read(contentLength))
+
+        segment = router.calculateNextSegment()
+        segmentIndex = 0 # 0 is segment leader
+        DBSegmentLeader = router.DBSegments[segment][segmentIndex] # 0 position is leader
+        while segmentIndex < len (router.DBSegments[segment]): # while DB replicas in this segment
+            try:
+                print(f"Trying with Node {segmentIndex}")
+                r = requests.post(f'http://{DBSegmentLeader}:7777', json=postData, timeout=5)
+                print(f"Success with Node {segmentIndex}")
+                break
+            except requests.exceptions.Timeout:
+                print (f"Node {segmentIndex} in segment {segment} not responding.", end=" ")
+                segmentIndex +=1 # use a replica
+        if segmentIndex == len(router.DBSegments):
+            raise SystemExit("No DB node in segment {segment} is responding.")
+        router.addKeyToSegmentValueTable(postData['key'], segment)
+        import time
+        time.sleep(3)
+        print(router.segmentValueTable)
+
 
         self.acknowledge()
         #self.wfile.write(f"AAA {post_data}")
-    
-    
-
 
 def run(server_class=HTTPServer, handler_class=RequestHandler, port=7777):
     server_address = ('', port)
-    router = Router()
     httpd = server_class(server_address, handler_class)
     try:
         httpd.serve_forever()
